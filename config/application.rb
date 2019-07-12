bc077f3952cb456479fd6298eea3d87d4934527c
Joao-Quaresma <joao.quaresma1991@gmail.com>
2019-07-12 08:04:18 +0100

    Fix prod images 4

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Yelpdemo
     config.generators.system_tests = nil
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.serve_static_assets = true
   end
 end
