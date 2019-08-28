c5eead5982cfc570911e9dd15b92b37d43891a9f
Joao-Quaresma <joao.quaresma1991@gmail.com>
2019-08-28 10:38:14 +0100

    add validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,9 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates :image, presence: true
+
   has_many :reviews
+
+  validates :image, :name, :address, :phone, :website, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/, message: 'must be in the format (123) 456-7890' }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/, message: 'must start with http:// or https://' }
 end
